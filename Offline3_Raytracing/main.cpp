490f0493951c5e598caf378e49e461c2a698b55b
smilodon <rakinkzs@gmail.com>
2023-08-31 10:22:04 +0600

    offline3 drawaing

--- Offline3_Raytracing/main.cpp
+++ Offline3_Raytracing/main.cpp
@@ -1,22 +1,23 @@
-#include <GL/glut.h>
 #include <bits/stdc++.h>
+#include <GL/glut.h>
+#include "utils.h"
 #include "bitmap_image.hpp" 
 
 
 using namespace std ;
     
-#define GLCOLOR(color) glColor3f(color.R/255.0,color.G/255.0,color.B/255.0)
+#define GLCOLOR(color) glColor3f( (color).R/255.0,(color).G/255.0,(color).B/255.0)
 #define TO_RADIAN(deg) ((deg)*(M_PI/180.0))
 #define DEG2RAD (M_PI/180.0) 
 
 
-struct point pos;   // position of the eye
-struct point l;     // look/forward direction
-struct point r;     // right direction
-struct point u;     // up direction
+Vector pos(200,0,10);   // position of the eye
+Vector l(-1/sqrt(2), -1/sqrt(2),0 );     // look/forward direction
+Vector r(-1/sqrt(2), 1/sqrt(2), 0);     // right direction
+Vector u = r.cross(l);     // up direction
+bool showAxes = 1;
 
-
-double near, far ; 
+double zNear, zFar ; 
 double fovY,fovX; 
 double aspect ;
 int recursion_level; 
@@ -25,13 +26,13 @@ int image_width;
 double checkerboard_side, checkerboard_ka, checkerboard_kd, checkerboard_ks, checkerboard_alpha ;
 vector<Object*> objects ;
 vector<Light*> lights; 
-vector<SpotLight*> spotlights; 
+vector<Spotlight*> spotlights; 
 
 
-void input(){
-    fstream in("description.txt", ios_base::in) ;
+void input(string filename){
+    fstream in(filename, ios_base::in) ;
 
-    in>>near>>far; 
+    in>>zNear>>zFar; 
     in>>fovY;     
     in>>aspect ;
     in>>recursion_level ;
@@ -39,6 +40,8 @@ void input(){
     image_width = image_height ;
     in>>checkerboard_side ;
     in>>checkerboard_ka>>checkerboard_kd>>checkerboard_ks; 
+    
+    int num_obj; 
     in>>num_obj ;
 
     objects.resize(num_obj) ;
@@ -56,7 +59,7 @@ void input(){
         }
         else if( obj_type == "sphere") {
             objects[i] = new Sphere() ; 
-            in>>*((Sphere)objects[i]); 
+            in>>*((Sphere*)objects[i]); 
         }
     }
 
@@ -73,18 +76,12 @@ void input(){
 	in >> num_spotlights;
 
 	for(int i=0;i<num_spotlights;i++){
-		Spotlight *spotlight = new SpotLight();
+		Spotlight *spotlight = new Spotlight();
 		in >> *spotlight;
 		spotlights.push_back(spotlight);
 	}
 }
 
-
-void initGL() {
-    // Set "clearing" or background color
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);   // Black and opaque
-    glEnable(GL_DEPTH_TEST);   // Enable depth testing for z-culling
-}
 
 
 void drawAxes() {
@@ -92,210 +89,40 @@ void drawAxes() {
     glBegin(GL_LINES);
         glColor3f(1,0,0);   // Red
         // X axis
-        glVertex3f(0,0,0);
-        glVertex3f(2,0,0);
+        glVertex3f(0,0,0);  glVertex3f(zFar,0,0);
 
         glColor3f(0,1,0);   // Green
         // Y axis
-        glVertex3f(0,0,0);        glVertex3f(0,1,0);
+        glVertex3f(0,0,0); glVertex3f(0,zFar,0);
 
         glColor3f(0,0,2);   // Blue
         // Z axis
-        glVertex3f(0,0,0);
-        glVertex3f(0,0,2);
+        glVertex3f(0,0,0); glVertex3f(0,0,zFar);
     glEnd();
 }
+
+
 void drawGrid(){
-    int i; 
-    glColor3f(0.6,0.6,0.6);
-    glBegin(GL_LINES);
-        for(i=-8;i<=8;i++){
-            glVertex3d(10*i,-90,0);
-            glVertex3d(10*i,90,0);
-
-            glVertex3d(-90,10*i,0);
-            glVertex3d(90,10*i,0);
+    int i = 0; 
+    int x = ceil(pos.x()/checkerboard_side)*checkerboard_side;
+    int y = ceil(pos.y()/checkerboard_side)*checkerboard_side;
+    int xL = x-zFar;
+    int yB = y-zFar;
+    int xR = x+zFar;
+    int yT = y+zFar;
+    for(x=xL;x<=xR;x+=checkerboard_side)
+        for(y=yB;y<=yT;y+=checkerboard_side){
+            if( ((int)(x/checkerboard_side) + (int)(y/checkerboard_side)) % 2 == 0 ) 
+                GLCOLOR(BLACK);
+            else GLCOLOR(WHITE);
+            glBegin(GL_QUADS);
+                glVertex3d(x,y,0);
+                glVertex3d(x+checkerboard_side,y,0);
+                glVertex3d(x+checkerboard_side,y+checkerboard_side,0);
+                glVertex3d(x,y+checkerboard_side,0);
+            glEnd();
         }
-    glEnd();
-}
-void generateVertices(){
-    double radius = 1.0 / (sin(DEG2RAD*sphereInscriptionAngle)+cos(DEG2RAD*sphereInscriptionAngle)); 
-    double n1[3];       
-    double n2[3];       
-    double v[3];         
-    double a1;        
-    double a2;  
-
-    // generate X+ vertices 
-    for(unsigned int i = 0; i < pointsPerRow; ++i){
-        a2 = DEG2RAD * (sphereInscriptionAngle - 2.0*sphereInscriptionAngle * i / (pointsPerRow - 1));
-        n2[0] = sin(a2);
-        n2[1] = 0;
-        n2[2] = cos(a2);
-        
-        for(unsigned int j = 0; j < pointsPerRow; ++j){
-            a1 = DEG2RAD * (sphereInscriptionAngle - 2.0 * sphereInscriptionAngle * j / (pointsPerRow - 1));
-            n1[0] = sin(a1);
-            n1[1] = cos(a1);
-            n1[2] = 0;
-
-            v[0] = n1[1] * n2[2] - n1[2] * n2[1];
-            v[1] = n1[2] * n2[0] - n1[0] * n2[2];
-            v[2] = n1[0] * n2[1] - n1[1] * n2[0];
-
-            // normalize direction vector
-            double scale = radius / sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]);
-            v[0] *= scale;
-            v[1] *= scale;
-            v[2] *= scale;
-
-            verticesX[i][j][0] = v[0];
-            verticesX[i][j][1] = v[1];
-            verticesX[i][j][2] = v[2];
-        }
-    }
-    // generate Y+ vertices 
-    for(int i=0;i<pointsPerRow;i++){
-        a2 = DEG2RAD * (sphereInscriptionAngle - 2.0*sphereInscriptionAngle*i/(pointsPerRow-1.0));
-        n2[0] = cos(a2);
-        n2[1] = sin(a2);
-        n2[2] = 0;           
-
-        for(int j=0;j<pointsPerRow;j++){
-            a1 = DEG2RAD * (sphereInscriptionAngle - 2.0*sphereInscriptionAngle*j/(pointsPerRow-1.0));
-            n1[0] = 0;
-            n1[1] = sin(a1); 
-            n1[2] = cos(a1);
-            
-            v[0] = n1[1]*n2[2] - n1[2]*n2[1];
-            v[1] = n1[2]*n2[0] - n1[0]*n2[2];
-            v[2] = n1[0]*n2[1] - n1[1]*n2[0];
-
-            // normalize direction vector
-            double scale = radius / sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]);
-            v[0] *= scale;
-            v[1] *= scale;
-            v[2] *= scale;
-
-            // add a vertex into array
-            verticesY[i][j][0] = v[0];
-            verticesY[i][j][1] = v[1];
-            verticesY[i][j][2] = v[2];
-        }
-    }
-
-    // generate Z+ vertices 
-    for(int i=0;i<pointsPerRow;i++){
-        a2 = DEG2RAD * (sphereInscriptionAngle - 2.0*sphereInscriptionAngle*i/(pointsPerRow-1.0));
-        n2[0] = 0; 
-        n2[1] = cos(a2);
-        n2[2] = sin(a2);
-        for(int j=0;j<pointsPerRow;j++){
-            a1 = DEG2RAD * (sphereInscriptionAngle - 2.0*sphereInscriptionAngle*j/(pointsPerRow-1.0));
-            n1[0] = cos(a1);
-            n1[1] = 0;  
-            n1[2] = sin(a1);
-
-            v[0] = n1[1]*n2[2] - n1[2]*n2[1];
-            v[1] = n1[2]*n2[0] - n1[0]*n2[2];
-            v[2] = n1[0]*n2[1] - n1[1]*n2[0];
-
-            // normalize direction vector
-            double scale = radius / sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]);
-            v[0] *= scale;
-            v[1] *= scale;
-            v[2] *= scale;
-
-            // add a vertex into array
-            verticesZ[i][j][0] = v[0];
-            verticesZ[i][j][1] = v[1];
-            verticesZ[i][j][2] = v[2];
-        }
-    }
-}
-
-void drawCubeSpherePart(vector<vector<vector<GLdouble> > > vertices){
-    glBegin(GL_QUAD_STRIP);
-    for(int i=0;i<pointsPerRow-1;i++){
-        for(int j=0;j<pointsPerRow;j++){
-            glVertex3d(vertices[i][j][0],vertices[i][j][1],vertices[i][j][2]);
-            glVertex3d(vertices[i+1][j][0],vertices[i+1][j][1],vertices[i+1][j][2]);
-        }
-    }
-    glEnd();
-}
-
-void drawSphereParts(){
-    GLCOLOR(RED);
-    drawCubeSpherePart(verticesX);
-    glPushMatrix();
-        glRotatef(180,0,1,0);
-        drawCubeSpherePart(verticesX) ;
-    glPopMatrix() ;
-
-    GLCOLOR(GREEN);
-    drawCubeSpherePart(verticesY);
-    glPushMatrix();
-        glRotatef(180,1,0,0);
-        drawCubeSpherePart(verticesY) ;
-    glPopMatrix() ;
-
-    GLCOLOR(BLUE);
-    drawCubeSpherePart(verticesZ);
-    glPushMatrix();
-        glRotatef(180,1,0,0);
-        drawCubeSpherePart(verticesZ) ;
-    glPopMatrix() ;
-}
-
-void drawTriangleParts(){
-    for(int i=0;i<8;i++){
-        if( i&1 ) GLCOLOR(CYAN);
-        else GLCOLOR(PINK) ;
-        glPushMatrix();
-        glRotatef( (i&3)*90.0,1,0,0);
-        if( i&4 ){
-            glRotatef(180.0,0,0,1);
-        }
-        glBegin(GL_TRIANGLES);
-            glVertex3d(verticesX[pointsPerRow-1][pointsPerRow-1][0],verticesX[pointsPerRow-1][pointsPerRow-1][1],verticesX[pointsPerRow-1][pointsPerRow-1][2]);
-            glVertex3d(verticesY[pointsPerRow-1][pointsPerRow-1][0],verticesY[pointsPerRow-1][pointsPerRow-1][1],verticesY[pointsPerRow-1][pointsPerRow-1][2]);
-            glVertex3d(verticesZ[pointsPerRow-1][pointsPerRow-1][0],verticesZ[pointsPerRow-1][pointsPerRow-1][1],verticesZ[pointsPerRow-1][pointsPerRow-1][2]);
-        glEnd();
-        glPopMatrix();
-    }
-}
-
-void drawCylinderPart(){
-    glBegin(GL_QUAD_STRIP);
-    for(int i=0;i<pointsPerRow;i++){
-        glVertex3d(verticesY[pointsPerRow-1][i][0],
-                verticesY[pointsPerRow-1][i][1],
-                verticesY[pointsPerRow-1][i][2]);
-        glVertex3d(verticesX[i][pointsPerRow-1][0],
-                verticesX[i][pointsPerRow-1][1],
-                verticesX[i][pointsPerRow-1][2]);
-    }
-    glEnd();
-}
-void drawCylinderParts(){
-    GLCOLOR(YELLOW);
-    for(int i=0;i<8;i++){
-        glPushMatrix();
-        glRotatef(90.0*(i&3),1,0,0);
-        if( i&4 ){
-            glRotatef(180,0,0,1);
-        }
-        drawCylinderPart();
-        glPopMatrix();
-    }
-    for(int i=0;i<4;i++){
-        glPushMatrix();
-            glRotatef(90*i,1,0,0);
-            glRotatef(90,0,1,0);
-            drawCylinderPart();
-        glPopMatrix();
-    }
+    
 }
 
 void display(void){
@@ -303,156 +130,129 @@ void display(void){
     glMatrixMode(GL_MODELVIEW);             // To operate on Model-View matrix
     glLoadIdentity();                       // Reset the model-view matrix
     
-    gluLookAt(pos.x,pos.y,pos.z,
-              pos.x+l.x,pos.y+l.y,pos.z+l.z,
-              u.x,u.y,u.z);
+    gluLookAt(pos.x(),pos.y(),pos.z(),
+              pos.x()+l.x(),pos.y()+l.y(),pos.z()+l.z(),
+              u.x(),u.y(),u.z());
 
-    drawAxes();
+    if( showAxes) drawAxes();
     drawGrid();
 
+    for(int i=0;i<objects.size();i++)
+        objects[i]->draw();
+    for(int i=0;i<lights.size();i++)
+        lights[i]->draw();
+    for(int i=0;i<spotlights.size();i++)
+        spotlights[i]->draw();
     glutSwapBuffers(); 
 }
 
 
-void keyboardListener(unsigned char key, int xx,int yy){
-    double rate = 0.01;
+
+void rotate(Vector &vec,Vector &axis,double angle){
+    vec = Matrix::R(vec,axis,angle);
+}
+
+
+void keyboardListener(unsigned char key, int x,int y){
+    double ROT_ANG = 15 ;
 	switch(key){
+		case '0':
+			// capture();
+			break;
 		case '1':
-			r.x = r.x*cos(rate)+l.x*sin(rate);
-			r.y = r.y*cos(rate)+l.y*sin(rate);
-			r.z = r.z*cos(rate)+l.z*sin(rate);
-
-			l.x = l.x*cos(rate)-r.x*sin(rate);
-			l.y = l.y*cos(rate)-r.y*sin(rate);
-			l.z = l.z*cos(rate)-r.z*sin(rate);
+			// rotate LEFT 
+			rotate(r,u,ROT_ANG);
+			rotate(l,u,ROT_ANG);
 			break;
-
-        case '2':
-			r.x = r.x*cos(-rate)+l.x*sin(-rate);
-			r.y = r.y*cos(-rate)+l.y*sin(-rate);
-			r.z = r.z*cos(-rate)+l.z*sin(-rate);
-
-			l.x = l.x*cos(-rate)-r.x*sin(-rate);
-			l.y = l.y*cos(-rate)-r.y*sin(-rate);
-			l.z = l.z*cos(-rate)-r.z*sin(-rate);
+		case '2':
+			// rotate right
+			rotate(r,u,-ROT_ANG);
+			rotate(l,u,-ROT_ANG);
 			break;
-
-        case '3':
-			l.x = l.x*cos(rate)+u.x*sin(rate);
-			l.y = l.y*cos(rate)+u.y*sin(rate);
-			l.z = l.z*cos(rate)+u.z*sin(rate);
-
-			u.x = u.x*cos(rate)-l.x*sin(rate);
-			u.y = u.y*cos(rate)-l.y*sin(rate);
-			u.z = u.z*cos(rate)-l.z*sin(rate);
+		case '3':
+			// rotate UP
+			rotate(u,r,ROT_ANG);
+			rotate(l,r,ROT_ANG);
 			break;
-
-        case '4':
-			l.x = l.x*cos(-rate)+u.x*sin(-rate);
-			l.y = l.y*cos(-rate)+u.y*sin(-rate);
-			l.z = l.z*cos(-rate)+u.z*sin(-rate);
-
-			u.x = u.x*cos(-rate)-l.x*sin(-rate);
-			u.y = u.y*cos(-rate)-l.y*sin(-rate);
-			u.z = u.z*cos(-rate)-l.z*sin(-rate);
+		case '4':
+			// rotate DOWN
+			rotate(u,r,-ROT_ANG);
+			rotate(l,r,-ROT_ANG);
 			break;
-
-        case '5':
-			u.x = u.x*cos(rate)+r.x*sin(rate);
-			u.y = u.y*cos(rate)+r.y*sin(rate);
-			u.z = u.z*cos(rate)+r.z*sin(rate);
-
-			r.x = r.x*cos(rate)-u.x*sin(rate);
-			r.y = r.y*cos(rate)-u.y*sin(rate);
-			r.z = r.z*cos(rate)-u.z*sin(rate);
+		case '5':
+			// tilt CLKWISE
+			rotate(r,l,ROT_ANG);
+			rotate(u,l,ROT_ANG);
 			break;
-
-        case '6':
-			u.x = u.x*cos(-rate)+r.x*sin(-rate);
-			u.y = u.y*cos(-rate)+r.y*sin(-rate);
-			u.z = u.z*cos(-rate)+r.z*sin(-rate);
-
-			r.x = r.x*cos(-rate)-u.x*sin(-rate);
-			r.y = r.y*cos(-rate)-u.y*sin(-rate);
-			r.z = r.z*cos(-rate)-u.z*sin(-rate);
-
+		case '6':
+			// tilt COUNTER CLKWISE
+			rotate(r,l,-ROT_ANG);
+			rotate(u,l,-ROT_ANG);
 			break;
-        case 'a': 
-            rot -= delRot; 
-            break; 
-        case 'd':
-            rot += delRot ;
-            break;   
-        case 'w':
-            pos.y += rate;
-            break;
-        case 's':
-            pos.y -= rate;
-            break;
-     
-        case ',' :
-            sphereInscriptionAngle += delSphereInscriptionAngle;
-            sphereInscriptionAngle = (sphereInscriptionAngle > 45.0) ? 45.0:sphereInscriptionAngle ;
-            break;
-        case '.':
-            sphereInscriptionAngle -= delSphereInscriptionAngle;
-            sphereInscriptionAngle = (sphereInscriptionAngle <= 0.0) ? 0.0:sphereInscriptionAngle ;
-            break;
-  
 		default:
 			break;
 	}
-	glutPostRedisplay();
+    glutPostRedisplay();
 }
 
+
 void specialKeyListener(int key, int x,int y){
+    double del = 3;
+    // cout<<pos<<endl; 
 	switch(key){
-		case GLUT_KEY_UP:		//down arrow key
-			pos.x+=l.x;
-			pos.y+=l.y;
-			pos.z+=l.z;
+		case GLUT_KEY_DOWN:		//down arrow key
+			pos = pos - l * del;
 			break;
-		case GLUT_KEY_DOWN:		// up arrow key
-			pos.x-=l.x;
-			pos.y-=l.y;
-			pos.z-=l.z;
+		case GLUT_KEY_UP:		// up arrow key
+			pos = pos + l * del;
 			break;
-
 		case GLUT_KEY_RIGHT:
-			pos.x+=r.x;
-			pos.y+=r.y;
-			pos.z+=r.z;
+			pos = pos + r * del;
 			break;
-		case GLUT_KEY_LEFT :
-			pos.x-=r.x;
-			pos.y-=r.y;
-			pos.z-=r.z;
+		case GLUT_KEY_LEFT:
+			pos = pos - r * del ; 
 			break;
-
 		case GLUT_KEY_PAGE_UP:
-		    pos.x+=u.x;
-			pos.y+=u.y;
-			pos.z+=u.z;
+			pos = pos + u * del;
 			break;
 		case GLUT_KEY_PAGE_DOWN:
-            pos.x-=u.x;
-			pos.y-=u.y;
-			pos.z-=u.z;
+			pos = pos - u * del ;
 			break;
-
 		case GLUT_KEY_INSERT:
 			break;
-
 		case GLUT_KEY_HOME:
 			break;
 		case GLUT_KEY_END:
 			break;
+		default:
+			break;
+	}
+    glutPostRedisplay();
+}
+
+
+void mouseListener(int button, int state, int x, int y){	
+	switch(button){
+		case GLUT_LEFT_BUTTON:
+			if(state == GLUT_DOWN){		
+				showAxes=1-showAxes;
+			}
+			break;
+
+		case GLUT_RIGHT_BUTTON:
+			//........
+			break;
+
+		case GLUT_MIDDLE_BUTTON:
+			//........
+			break;
 
 		default:
 			break;
 	}
-	glutPostRedisplay();
+    glutPostRedisplay();
 }
+
 
 void reshapeListener(GLsizei width, GLsizei height) {  // GLsizei for non-negative integer
     // Compute aspect ratio of the new window
@@ -473,25 +273,36 @@ void reshapeListener(GLsizei width, GLsizei height) {  // GLsizei for non-negati
         gluOrtho2D(-1.0, 1.0, -1.0 / aspect, 1.0 / aspect);
     }*/
     // Enable perspective projection with fovy, aspect, zNear and zFar
-    gluPerspective(45.0f, aspect, 0.1f, 100.0f);
+    gluPerspective(fovY, aspect, zNear, zFar);
 }
-int main(int argc, char** argv)
-{
-    pos.x=0;pos.y=0;pos.z=10;
 
-    l.x=0;l.y=0;l.z=-1;
-    u.x=0;u.y=1;u.z=0;
-    r.x=0.25;r.y=0;r.z=0;
+void initGL() {
+    // Set "clearing" or background color
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);   // Black and opaque
+    glEnable(GL_DEPTH_TEST);   // Enable depth testing for z-culling
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(fovY, aspect, zNear, zFar);
+}
+
+
+int main(int argc, char** argv){
+    if( argc > 1 )
+        input(argv[1]) ;
+    else input("description.txt") ;
+
     glutInit(&argc, argv);
     // glutInitDisplayMode(GLUT_SINGLE);
     glutInitWindowSize(1024, 1024);
     glutInitWindowPosition(100, 100);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB);	//Depth, Double buffer, RGB color
-    glutCreateWindow("Cube");
+    glutCreateWindow("Ray Tracing");
     glutDisplayFunc(display);
     glutReshapeFunc(reshapeListener);           // Register callback handler for window re-shape
     glutSpecialFunc(specialKeyListener);
     glutKeyboardFunc(keyboardListener);
+    glutMouseFunc(mouseListener);
     initGL();
     glutMainLoop();
     return 0;
